1d6ec641336ea4974208b3027fbae24723dfc109
russ <820377596@qq.com>
2023-10-28 10:00:05 +0800

    if

--- exercises/if/if2.rs
+++ exercises/if/if2.rs
@@ -10,9 +10,11 @@
 pub fn foo_if_fizz(fizzish: &str) -> &str {
     if fizzish == "fizz" {
         "foo"
+    } else if fizzish == "fuzz" {
+        "bar"
     } else {
-        1
-    }
+        "baz"
+}
 }
 
 // No test changes needed!
