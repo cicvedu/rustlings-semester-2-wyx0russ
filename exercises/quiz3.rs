4fa4efdc92ed325f39c9ba086c90d28daafdc34c
russ <820377596@qq.com>
2023-11-04 08:44:51 +0800

    traits

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,18 +16,33 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+pub trait print {
+    fn print(&self) -> String;
+}
 
-pub struct ReportCard {
+
+pub struct grade {
     pub grade: f32,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
-    pub fn print(&self) -> String {
+pub struct student_record {
+    pub student_name: String,
+    pub student_age: u8,
+    pub student_record: String,
+}
+
+impl print for grade {
+    fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
+    }
+}
+impl print for student_record {
+    fn print(&self) -> String {
+        format!("{} ({}) - achieved a grade of {}",
+            &self.student_name, &self.student_age, &self.student_record)
     }
 }
 
@@ -37,7 +52,7 @@ mod tests {
 
     #[test]
     fn generate_numeric_report_card() {
-        let report_card = ReportCard {
+        let report_card = grade {
             grade: 2.1,
             student_name: "Tom Wriggle".to_string(),
             student_age: 12,
@@ -51,8 +66,9 @@ mod tests {
     #[test]
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
-        let report_card = ReportCard {
-            grade: 2.1,
+
+        let report_card = student_record {
+            student_record: "A+".to_string(),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
